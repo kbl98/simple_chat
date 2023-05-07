023839ac1b96ff9528c469930ef5b4e3cfa3ef1e
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-05-07 21:45:13 +0200

    chat 2.0

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Chat,Message
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         print('This request:'+request.POST['textmessage'])
-    return render(request,'chats/index.html',{'username':'Katja'})
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text = request.POST['textmessage'],author=request.user, receiver = request.user, chat = myChat)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request,'chats/index.html',{'username':'Katja','messages':chatMessages})
